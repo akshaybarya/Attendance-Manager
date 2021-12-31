7ff7a88bad2806cbdc6268f9d3a8d217c191eb88
akshaybarya <akshaybarya@gmail.com>
2021-12-31 12:27:03 +0530

    Production

--- server.js
+++ server.js
@@ -210,6 +210,18 @@ app.post(`/teacher_logs`, auth, async (req, res) => {
   res.status(200).send(Logs2[req.body.id]);
 });
 
-app.listen(5000, () => {
-  console.log("listening on port: 5000");
+//server static assets in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`listening on port: ${PORT}`);
 });
